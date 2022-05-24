13e53852b11fcea872a9313d8cd2cb07276f537d
gparrag <92894022+gparrag@users.noreply.github.com>
2022-05-24 08:12:15 -0500

    add preprocess function

--- Model/Movies_Model.py
+++ Model/Movies_Model.py
@@ -8,10 +8,38 @@ import sys
 import os
 import warnings
 warnings.filterwarnings('ignore')
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+nltk.download('punkt')
+nltk.download('stopwords')
+warnings.filterwarnings('ignore')
 
 cols = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family',
         'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Romance',
         'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
+
+def preprocess(df_text):
+    df_text=df_text.lower()
+    df_text=df_text.replace("'","")
+    df_text=df_text.replace("“","")
+    df_text=df_text.replace("”","")
+    df_text=df_text.replace('—','')
+    df_text=df_text.replace('’','')
+    df_text=df_text.replace('‘','')
+    df_text=re.sub(" \d+", " ",df_text)
+    df_text=re.sub("\\W"," ",df_text)
+    df_text=re.sub("/\b(\w)\b/","",df_text)
+    df_text = re.sub('<[^>]*>', '', df_text)# remove none alphanumeric text
+    df_text = re.sub('<.*?>+', '', df_text)
+    df_text = re.sub(r"\s+[a-zA-Z]\s+", ' ', df_text)
+    df_text = re.sub('[%s]' % re.escape(string.punctuation), '', df_text)
+    tokenized_list = nltk.word_tokenize(df_text)
+    stop_words=set(stopwords.words('english')) 
+    cleaned_list=[i for i in tokenized_list if i not in stop_words]
+    return ' '.join(cleaned_list)
+
 
 def predict_genre(plot):
     #load model
@@ -23,6 +51,7 @@ def predict_genre(plot):
     #embeded model
     model_embed = SentenceTransformer('bert-base-uncased')
     model_embed.max_seq_length = 110
+    plot=preprocess(plot)
     plot_embed = model_embed.encode(plot)
     plot_embed = plot_embed.reshape(1,1,plot_embed.shape[0])
     genres= loaded_model.predict(plot_embed)
